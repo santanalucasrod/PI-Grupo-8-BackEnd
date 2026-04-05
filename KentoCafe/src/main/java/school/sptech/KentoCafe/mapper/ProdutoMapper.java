a65ca3884400a23e6174cd8735853d009ce39de7
santanalucasrod <lucas.rodrigues127@sptech.school>
2026-04-05 18:20:43 -0300

    adicionando mapper de produto

---
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/mapper/ProdutoMapper.java
@@ -0,0 +1,31 @@
+package school.sptech.KentoCafe.mapper;
+
+import school.sptech.KentoCafe.dto.produto.ProdutoRequestDto;
+import school.sptech.KentoCafe.dto.produto.ProdutoResponseDto;
+import school.sptech.KentoCafe.entity.Categoria;
+import school.sptech.KentoCafe.entity.Produto;
+
+public class ProdutoMapper {
+
+    public static Produto toEntity(ProdutoRequestDto dto, Categoria categoria) {
+        Produto produto = new Produto();
+        produto.setNome(dto.getNome());
+        produto.setCategoria(categoria);
+        produto.setPrecoUnidade(dto.getPrecoUnidade());
+        produto.setDescricao(dto.getDescricao());
+        produto.setPathFt(dto.getPathFt());
+        return produto;
+    }
+
+    public static ProdutoResponseDto toResponseDTO(Produto produto) {
+        ProdutoResponseDto dto = new ProdutoResponseDto();
+        dto.setId(produto.getId());
+        dto.setNome(produto.getNome());
+        dto.setCategoriaNome(produto.getCategoria().getNome());
+        dto.setCategoriaId(produto.getCategoria().getId());
+        dto.setPrecoUnidade(produto.getPrecoUnidade());
+        dto.setDescricao(produto.getDescricao());
+        dto.setPathFt(produto.getPathFt());
+        return dto;
+    }
+}
