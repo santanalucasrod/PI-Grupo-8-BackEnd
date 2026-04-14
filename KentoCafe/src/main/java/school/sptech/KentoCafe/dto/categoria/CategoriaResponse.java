36bfc9cb2b361ecaa0e4a229d9fe6e61c27178be
santanalucasrod <lucas.rodrigues127@sptech.school>
2026-04-14 18:04:24 -0300

    feat: response de categoria

--- KentoCafe/src/main/java/school/sptech/KentoCafe/dto/categoria/CategoriaResponse.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/dto/categoria/CategoriaResponse.java
@@ -1,4 +1,20 @@
 package school.sptech.KentoCafe.dto.categoria;
 
+import school.sptech.KentoCafe.dto.produto.ProdutoResponse;
+import java.util.List;
+
 public class CategoriaResponse {
+
+    private Integer id;
+    private String nome;
+    private List<ProdutoResponse> produtos;
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public List<ProdutoResponse> getProdutos() { return produtos; }
+    public void setProdutos(List<ProdutoResponse> produtos) { this.produtos = produtos; }
 }
