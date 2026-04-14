f82e985db5e51e30424212954e46d859f439e2bd
santanalucasrod <lucas.rodrigues127@sptech.school>
2026-04-14 18:08:24 -0300

    feat: mapeando categoria para toentity e toresponse

--- KentoCafe/src/main/java/school/sptech/KentoCafe/mapper/CategoriaMapper.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/mapper/CategoriaMapper.java
@@ -1,4 +1,29 @@
 package school.sptech.KentoCafe.mapper;
 
+import school.sptech.KentoCafe.dto.categoria.CategoriaRequest;
+import school.sptech.KentoCafe.dto.categoria.CategoriaResponse;
+import school.sptech.KentoCafe.entity.Categoria;
+import school.sptech.KentoCafe.entity.Produto;
+
+import java.util.List;
+
 public class CategoriaMapper {
+
+    public static Categoria toEntity(CategoriaRequest dto) {
+        Categoria categoria = new Categoria();
+        categoria.setNome(dto.getNome());
+        return categoria;
+    }
+
+    public static CategoriaResponse toResponse(Categoria categoria, List<Produto> produtos) {
+        CategoriaResponse dto = new CategoriaResponse();
+        dto.setId(categoria.getId());
+        dto.setNome(categoria.getNome());
+        dto.setProdutos(
+                produtos.stream()
+                        .map(ProdutoMapper::toResponseDTO)
+                        .toList()
+        );
+        return dto;
+    }
 }
