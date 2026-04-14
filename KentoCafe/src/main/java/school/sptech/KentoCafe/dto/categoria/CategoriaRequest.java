c1f11ada5d4f5ecfc70a5535a7674ac65e680715
santanalucasrod <lucas.rodrigues127@sptech.school>
2026-04-14 18:01:42 -0300

    feat: categoria request

--- KentoCafe/src/main/java/school/sptech/KentoCafe/dto/categoria/CategoriaRequest.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/dto/categoria/CategoriaRequest.java
@@ -1,4 +1,14 @@
 package school.sptech.KentoCafe.dto.categoria;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class CategoriaRequest {
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 50, message = "Nome deve ter no máximo 50 caracteres")
+    private String nome;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 }
