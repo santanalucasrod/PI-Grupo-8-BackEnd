0fc862ac8df20a53f920f64b157c003addd3cb66
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-04-21 03:04:35 -0300

    feat: repository e service de pedidos: implementação da lógica

---
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/service/PedidoService.java
@@ -0,0 +1,58 @@
+package school.sptech.KentoCafe.service;
+
+import jakarta.transaction.Transactional;
+import school.sptech.KentoCafe.dto.pedido.item.ItemRequest;
+import school.sptech.KentoCafe.dto.pedido.pedido.PedidoRequest;
+import school.sptech.KentoCafe.dto.pedido.pedido.PedidoResponse;
+import school.sptech.KentoCafe.entity.Pedido;
+import school.sptech.KentoCafe.entity.Produto;
+import school.sptech.KentoCafe.entity.Venda;
+import school.sptech.KentoCafe.exceptions.CarrinhoVazioException;
+import school.sptech.KentoCafe.exceptions.ProdutoNaoEncontradoException;
+import school.sptech.KentoCafe.mapper.PedidoMapper;
+import school.sptech.KentoCafe.repository.PedidoRepository;
+import school.sptech.KentoCafe.repository.ProdutoRepository;
+
+import java.time.LocalDateTime;
+
+public class PedidoService {
+    private final PedidoRepository pedidoRepository;
+    private final ProdutoRepository produtoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository, ProdutoRepository produtoRepository) {
+        this.pedidoRepository = pedidoRepository;
+        this.produtoRepository = produtoRepository;
+    }
+
+    @Transactional
+    public PedidoResponse finalizarPedido(PedidoRequest request) {
+        if (request.getItens() == null || request.getItens().isEmpty()) {
+            throw new CarrinhoVazioException();
+        }
+
+        Pedido novoPedido = new Pedido();
+        novoPedido.setDtHrPedido(LocalDateTime.now());
+        novoPedido.setStatus("PENDENTE");
+        novoPedido.setInfoAdicional(request.getInfoAdicional());
+
+        double totalAcumulado = 0.0;
+
+        for (ItemRequest item : request.getItens()) {
+            Produto p = produtoRepository.findById(item.getProdutoId())
+                    .orElseThrow(ProdutoNaoEncontradoException::new);
+
+            Venda venda = new Venda();
+            venda.setProduto(p);
+            venda.setQuantidade(item.getQuantidade());
+            venda.setPedido(novoPedido);
+
+            totalAcumulado += p.getPrecoUnidade() * item.getQuantidade();
+
+            novoPedido.getItens().add(venda);
+        }
+
+        Pedido pedidoSalvo = pedidoRepository.save(novoPedido);
+        return PedidoMapper.toResponse(pedidoSalvo, totalAcumulado);
+    }
+
+}
