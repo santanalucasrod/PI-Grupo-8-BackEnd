43d6866f0e0e09961c4f22ebfb5096cacf5a7384
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-05-18 19:42:08 -0300

    feat: criando repositório da nova entity InfoAdicional

--- KentoCafe/src/main/java/school/sptech/KentoCafe/repository/InfoAdicionalRepository.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/repository/InfoAdicionalRepository.java
@@ -1,4 +1,9 @@
 package school.sptech.KentoCafe.repository;
 
-public class InfoAdicionalRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import school.sptech.KentoCafe.entity.InfoAdicional;
+
+@Repository
+public interface InfoAdicionalRepository extends JpaRepository<InfoAdicional, Integer> {
 }
