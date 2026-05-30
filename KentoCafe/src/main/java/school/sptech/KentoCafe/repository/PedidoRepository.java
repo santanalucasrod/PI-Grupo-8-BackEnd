f54e260ac2c9432b33a352cca59b43e0b5474cf8
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-05-30 14:39:15 -0300

    fix: adicionando lógica de pedido

--- KentoCafe/src/main/java/school/sptech/KentoCafe/repository/PedidoRepository.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/repository/PedidoRepository.java
@@ -1,7 +1,14 @@
 package school.sptech.KentoCafe.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import school.sptech.KentoCafe.entity.Pedido;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+import java.util.List;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByStatusNome(String statusNome);
+
+    List<Pedido> findByFuncionarioId(Integer funcionarioId);
 }
