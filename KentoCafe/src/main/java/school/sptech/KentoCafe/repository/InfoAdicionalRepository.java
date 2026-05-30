283faf0c781f86da41c3627a9511f980f76ced36
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-05-30 14:13:31 -0300

    fix: deletando arquivos não mais utilizados

--- KentoCafe/src/main/java/school/sptech/KentoCafe/repository/InfoAdicionalRepository.java
+++
@@ -1,9 +0,0 @@
-package school.sptech.KentoCafe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import school.sptech.KentoCafe.entity.InfoAdicional;
-
-@Repository
-public interface InfoAdicionalRepository extends JpaRepository<InfoAdicional, Integer> {
-}
---
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/security/JwtFilter.java
@@ -0,0 +1,4 @@
+package school.sptech.KentoCafe.security;
+
+public class JwtFilter {
+}
