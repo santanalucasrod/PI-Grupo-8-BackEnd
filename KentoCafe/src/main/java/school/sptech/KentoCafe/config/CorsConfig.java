5dc7cb3c93f28897c957d978ff009c919efa6da5
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-08-23 16:24:56 -0300

    feat: OWASP (A02:2025) Security Misconfiguration / Cross-Site Request Forgery (CSRF)

--- KentoCafe/src/main/java/school/sptech/KentoCafe/config/CorsConfig.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
