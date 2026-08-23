e43e118bacd521adc5d7ac980a5f528ce410ef1d
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-08-23 16:32:33 -0300

    feat: OWASP: A09:2025 – Security Logging and Monitoring Failures (Falhas de Log e Monitoramento de Segurança)

--- KentoCafe/src/main/java/school/sptech/KentoCafe/security/UserDetailsServiceImpl.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/security/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package school.sptech.KentoCafe.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +11,7 @@ import school.sptech.KentoCafe.repository.FuncionarioRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
     private final FuncionarioRepository repository;
 
     public UserDetailsServiceImpl(FuncionarioRepository repository) {
@@ -20,8 +23,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         Funcionario funcionario = repository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Funcionário não encontrado: " + email));
 
-        System.out.println("✅ Usuário encontrado: " + funcionario.getEmail());
-        System.out.println("✅ Senha no banco: " + funcionario.getSenha());
+        log.debug("Dados do usuário {} carregados com sucesso para o contexto de segurança.", email);
 
         return funcionario;
     }
