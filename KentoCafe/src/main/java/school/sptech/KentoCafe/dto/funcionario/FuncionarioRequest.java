beba998c462dcfd79783f16f78656e08bb7553b6
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-08-23 21:53:25 -0300

    feat: OWASP: A06:2025 – Insecure Design

--- KentoCafe/src/main/java/school/sptech/KentoCafe/dto/funcionario/FuncionarioRequest.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/dto/funcionario/FuncionarioRequest.java
@@ -1,22 +1,30 @@
 package school.sptech.KentoCafe.dto.funcionario;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "Dados para cadastro ou atualização de funcionário")
 public class FuncionarioRequest {
+
+    @NotBlank(message = "O nome não pode estar vazio")
+    @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
     @Schema(description = "Nome completo do funcionário", example = "João Silva")
     private String nome;
 
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 8, max = 64, message = "A senha deve ter no mínimo 8 caracteres")
     @Schema(description = "Senha de acesso", example = "senha123")
     private String senha;
 
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
     @Schema(description = "E-mail do funcionário", example = "joao@kentocafe.com")
     private String email;
 
+    @NotNull(message = "É necessário informar se o usuário é gerente")
     @Schema(description = "Indica se o funcionário é gerente", example = "false")
     private Boolean gerente;
 
