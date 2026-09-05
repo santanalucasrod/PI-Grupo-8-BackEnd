97df6cd23ecbe06b77edcf2e08eb6ac5bdf2dab8
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-09-05 01:46:24 -0300

    feat: adicionando busca por funcionario ativo

--- KentoCafe/src/main/java/school/sptech/KentoCafe/repository/FuncionarioRepository.java
+++ KentoCafe/src/main/java/school/sptech/KentoCafe/repository/FuncionarioRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.sptech.KentoCafe.entity.Funcionario;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
     Optional<Funcionario> findByEmail(String email);
+
+    List<Funcionario> findByAtivoTrue();
 }
