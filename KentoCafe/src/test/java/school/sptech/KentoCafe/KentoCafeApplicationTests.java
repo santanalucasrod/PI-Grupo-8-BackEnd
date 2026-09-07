4043d5585de99f9d7f274723fed983cc10c5d120
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-09-07 13:56:55 -0300

    feat: trazendo banco de teste h2 para a esteira visto impeditivo de acesso

--- KentoCafe/src/test/java/school/sptech/KentoCafe/KentoCafeApplicationTests.java
+++ KentoCafe/src/test/java/school/sptech/KentoCafe/KentoCafeApplicationTests.java
@@ -2,8 +2,10 @@ package school.sptech.KentoCafe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KentoCafeApplicationTests {
 
 	@Test
