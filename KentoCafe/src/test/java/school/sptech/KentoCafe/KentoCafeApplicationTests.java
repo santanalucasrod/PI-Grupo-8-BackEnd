90c696ba2cd09cfc2a8f207edcc2ce36c6a3fd12
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2026-09-07 14:07:10 -0300

    fix: sem conexão fisica com o JDBC, apenas uma simulação

--- KentoCafe/src/test/java/school/sptech/KentoCafe/KentoCafeApplicationTests.java
+++ KentoCafe/src/test/java/school/sptech/KentoCafe/KentoCafeApplicationTests.java
@@ -2,11 +2,17 @@ package school.sptech.KentoCafe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class KentoCafeApplicationTests {
+
+	@MockBean
+    private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
